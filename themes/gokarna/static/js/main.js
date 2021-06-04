ea3f003e10f363e84ab75b6c9584b5e2693cb924
Avijit 🚀 <526avijit@gmail.com>
2021-06-04 14:45:49 +0200

    Add dark light theme toggle functionality

--- themes/gokarna/static/js/main.js
+++ themes/gokarna/static/js/main.js
@@ -1,11 +1,29 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 100) {
-        document.querySelectorAll('.header, .header-nav').forEach(function(item) {
-            item.classList.add('small')
-        })
-    } else {
-        document.querySelectorAll('.header, .header-nav').forEach(function(item) {
-            item.classList.remove('small')
-        })
-    }
-});
+window.onload = () => {
+    const GOKARNA_LIGHT_THEME = 'gokarna-light';
+    const GOKARNA_DARK_THEME = 'gokarna-dark';
+    const themeClassSwitcherMap = {
+        [GOKARNA_LIGHT_THEME]: GOKARNA_DARK_THEME,
+        [GOKARNA_DARK_THEME]: GOKARNA_LIGHT_THEME
+    };
+
+    document.getElementById('dark-light-theme-toggle').addEventListener('click', () => {
+        const currentTheme = document.querySelector('html').classList[0];
+        const themeToSwitchTo = themeClassSwitcherMap[currentTheme];
+        document.querySelectorAll(`.${currentTheme}`).forEach((element) => {
+            element.classList.add(themeToSwitchTo);
+            element.classList.remove(currentTheme);
+        });
+    });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            document.querySelectorAll('.header, .header-nav').forEach(function(item) {
+                item.classList.add('small')
+            })
+        } else {
+            document.querySelectorAll('.header, .header-nav').forEach(function(item) {
+                item.classList.remove('small')
+            })
+        }
+    });
+}
