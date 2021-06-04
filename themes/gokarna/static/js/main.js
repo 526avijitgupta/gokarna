1f0818e5f0e0b30b66e039e2f1ef7a7367e17aca
Avijit 🚀 <526avijit@gmail.com>
2021-06-04 16:43:45 +0200

    Fix theme switching and templating

--- themes/gokarna/static/js/main.js
+++ themes/gokarna/static/js/main.js
@@ -1,29 +1,46 @@
-window.onload = () => {
-    const GOKARNA_LIGHT_THEME = 'gokarna-light';
-    const GOKARNA_DARK_THEME = 'gokarna-dark';
-    const themeClassSwitcherMap = {
-        [GOKARNA_LIGHT_THEME]: GOKARNA_DARK_THEME,
-        [GOKARNA_DARK_THEME]: GOKARNA_LIGHT_THEME
-    };
+document.addEventListener('DOMContentLoaded', ready, false);
 
-    document.getElementById('dark-light-theme-toggle').addEventListener('click', () => {
-        const currentTheme = document.querySelector('html').classList[0];
-        const themeToSwitchTo = themeClassSwitcherMap[currentTheme];
-        document.querySelectorAll(`.${currentTheme}`).forEach((element) => {
-            element.classList.add(themeToSwitchTo);
-            element.classList.remove(currentTheme);
-        });
-    });
+const THEME_PREF_STORAGE_KEY = "theme-preference";
+const GOKARNA_LIGHT_THEME = 'gokarna-light';
+const GOKARNA_DARK_THEME = 'gokarna-dark';
+let toggleIcon = '';
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
-            document.querySelectorAll('.header, .header-nav').forEach(function(item) {
-                item.classList.add('small')
-            })
-        } else {
-            document.querySelectorAll('.header, .header-nav').forEach(function(item) {
-                item.classList.remove('small')
-            })
+function ready() {
+    toggleIcon = document.querySelector('#dark-theme-toggle span');
+    // TODO: Fetch programatically by user's system preference
+    const savedTheme = localStorage.getItem(THEME_PREF_STORAGE_KEY) || GOKARNA_LIGHT_THEME;
+    setTheme(savedTheme);
+    document.getElementById('dark-theme-toggle').addEventListener('click', () => {
+        if (toggleIcon.className === "fa fa-moon-o") {
+            setTheme(GOKARNA_DARK_THEME);
+        } else if (toggleIcon.className === "fa fa-sun-o") {
+            setTheme(GOKARNA_LIGHT_THEME);
         }
     });
 }
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        document.querySelectorAll('.header, .header-nav').forEach(function(item) {
+            item.classList.add('small')
+        })
+    } else {
+        document.querySelectorAll('.header, .header-nav').forEach(function(item) {
+            item.classList.remove('small')
+        })
+    }
+});
+
+function setTheme(themeToSet) {
+
+    const darkThemeCss = document.getElementById("dark-theme");
+    localStorage.setItem(THEME_PREF_STORAGE_KEY, themeToSet);
+    if (themeToSet === GOKARNA_DARK_THEME) {
+        darkThemeCss.disabled = false;
+        toggleIcon.className = "fa fa-sun-o";
+    } else if(themeToSet === GOKARNA_LIGHT_THEME) {
+        darkThemeCss.disabled = true;
+        toggleIcon.className = "fa fa-moon-o";
+    }
+}
+
