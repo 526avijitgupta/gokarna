a252f2fd38bede0bdc11b793db1ad8ce9994a7df
Avijit 🚀 <526avijit@gmail.com>
2021-06-15 18:43:33 +0200

    Replace fa by feather icons

--- themes/gokarna/static/js/main.js
+++ themes/gokarna/static/js/main.js
@@ -2,23 +2,24 @@ document.addEventListener('DOMContentLoaded', ready, false);
 
 const THEME_PREF_STORAGE_KEY = "theme-preference";
 const THEME_TO_ICON_CLASS = {
-    'dark': 'fa fa-sun-o',
-    'light':'fa fa-moon-o'
+    'dark': 'feather-moon',
+    'light':'feather-sun'
 };
 let toggleIcon = '';
 let darkThemeCss = '';
 
 function ready() {
-    toggleIcon = document.querySelector('#dark-theme-toggle span');
+    feather.replace({ 'stroke-width': 1, width: 20, height: 20 });
     darkThemeCss = document.getElementById("dark-theme");
 
     const savedTheme = localStorage.getItem(THEME_PREF_STORAGE_KEY) || 
         (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark': 'light');
     setTheme(savedTheme);
     document.getElementById('dark-theme-toggle').addEventListener('click', () => {
-        if (toggleIcon.className === THEME_TO_ICON_CLASS.dark) {
+        toggleIcon = document.querySelector("#dark-theme-toggle a svg.feather");
+        if (toggleIcon.classList[1] === THEME_TO_ICON_CLASS.dark) {
             setTheme('light');
-        } else if (toggleIcon.className === THEME_TO_ICON_CLASS.light) {
+        } else if (toggleIcon.classList[1] === THEME_TO_ICON_CLASS.light) {
             setTheme('dark');
         }
     });
@@ -39,6 +40,6 @@ window.addEventListener('scroll', () => {
 function setTheme(themeToSet) {
     localStorage.setItem(THEME_PREF_STORAGE_KEY, themeToSet);
     darkThemeCss.disabled = themeToSet === 'light';
-    toggleIcon.className = THEME_TO_ICON_CLASS[themeToSet];
+    document.querySelector('#dark-theme-toggle a').innerHTML = feather.icons[THEME_TO_ICON_CLASS[themeToSet].split('-')[1]].toSvg();
 }
 
