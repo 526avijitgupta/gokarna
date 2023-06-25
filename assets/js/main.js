a02ae0057d246f18d43f1c5a9025ccf1687c7272
David Kröll <david.kroell@outlook.com>
2023-06-25 16:28:41 +0200

    Attach event listener with options (#174)

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,14 +110,16 @@ function setThemeByUserPref() {
         (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark': 'light');
     const darkThemeToggles = document.querySelectorAll('.dark-theme-toggle');
     setTheme(savedTheme, darkThemeToggles);
-    darkThemeToggles.forEach(el => el.addEventListener('click', (event) => {
-        toggleIcon = event.currentTarget.querySelector("a svg.feather");
-        if (toggleIcon.classList[1] === THEME_TO_ICON_CLASS.dark) {
-            setTheme('light', [event.currentTarget]);
-        } else if (toggleIcon.classList[1] === THEME_TO_ICON_CLASS.light) {
-            setTheme('dark', [event.currentTarget]);
-        }
-    }));
+    darkThemeToggles.forEach(el => el.addEventListener('click', toggleTheme, {capture: true}))
+}
+
+function toggleTheme(event) {
+    toggleIcon = event.currentTarget.querySelector("a svg.feather");
+    if (toggleIcon.classList[1] === THEME_TO_ICON_CLASS.dark) {
+        setTheme('light', [event.currentTarget]);
+    } else if (toggleIcon.classList[1] === THEME_TO_ICON_CLASS.light) {
+        setTheme('dark', [event.currentTarget]);
+    }
 }
 
 function setTheme(themeToSet, targets) {
